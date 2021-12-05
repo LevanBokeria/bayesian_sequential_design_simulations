70b3abf4c8c83b7a150834f7abea112b0dacc990
Levan Bokeria <levan.bokeria@gmail.com>
2021-12-05 19:22:09 +0400

    renamed the raw data folder. Improving the preprocessing script

--- 2_preprocess_slurm_output.R
+++ 2_preprocess_slurm_output.R
@@ -5,28 +5,35 @@
 
 # The created dataframe can then be used to do whatever summary statistics you want to do on it.
 
-library(data.table)
-library(rio)
+# Setup global parameters #####################################################
+pacman::p_load(data.table,
+               tidyverse,
+               rio)
 
 saveDF <- T
 
 folder <- 'try_1'
 
-filename <- paste(
-        './_rslurm_', 
+filename <- paste('./data/_rslurm_', 
         folder,
         '/results_0.RDS',sep = '')
 
-saveName <- paste(
-        './_rslurm_',
-        folder,
-        '/sims_preprocessed.RData',
-        sep = '')
+saveFolder <- file.path('./analysis_output/preprocessing',
+                        folder)
 
+saveName <- paste(saveFolder,
+                  '/sims_preprocessed.RData',
+                  sep = '')
+
+# If the save directory doesn't exist, create it
+ifelse(!dir.exists(saveFolder), dir.create(saveFolder, recursive = T), 'Save directory already exists!')
+
+# Import the file and concatenate #############################################
 tempList <- import(filename)
 
 sims_preprocessed <- rbindlist(tempList, idcol = 'id')
 
+# Save the file #####################################################
 if (saveDF){
         save(sims_preprocessed, file = saveName)
 }
