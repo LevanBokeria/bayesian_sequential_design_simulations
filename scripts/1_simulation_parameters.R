f022addb09c2ed1a942d5a010a54951225d1a636
Levan Bokeria <levan.bokeria@gmail.com>
2022-09-28 13:21:03 +0100

    some changes I do not remember

--- scripts/1_simulation_parameters.R
+++ scripts/1_simulation_parameters.R
@@ -30,24 +30,23 @@ nIter         <- 10000
 # Sequential design parameters. 
 # For d, crit1, and crit2 you can enter a vector of numbers.
 
-nLimit    <- 400 # maximum number of participants to run
+nLimit    <- 200 # maximum number of participants to run
 d         <- c(0,
-               0.84,
-               1.72) # various effect sizes to consider
-crit1     <- c(6,10) # criteria for stopping for BF10
+               1) # various effect sizes to consider
+crit1     <- c(6) # criteria for stopping for BF10
 crit2     <- c(1/6) # criteria for stopping for BF01
-minN      <- 20 # Initial minimum number of participants per group
-batchSize <- 10 # How many participants to add per group when neither of the criteria are reached.
+minN      <- 24 # Initial minimum number of participants per group
+batchSize <- 8 # How many participants to add per group when neither of the criteria are reached.
 
 # Note: if various batchSizes are simulated the post-processing scripts
 # might not work.
 
 # What type of test is it?
-test_types <- c('unpaired','paired')
-side_types <- c('two_tailed','one_tailed')
+test_types <- c('unpaired')
+side_types <- c('two_tailed')
 
 # Name for saving folder
-saveFolder <- 'results_density_across_choose_sparse'
+saveFolder <- 'results_generalization'
 
 # Submit the slurm job?
 submitJob <- T
--- scripts/6_post_slurm_wrapper.R
+++ scripts/6_post_slurm_wrapper.R
@@ -12,12 +12,12 @@ source('./scripts/5_plot_results.R')
 saveData <- TRUE
 
 # For the summary stats
-nFrom <- 20
-nTo   <- 400
-nBy   <- 10
+nFrom <- 24
+nTo   <- 200
+nBy   <- 8
 
 # Folder where the slurm output is
-folder <- 'results_density_across_choose_sparse'
+folder <- 'results_generalization'
 
 # Now, call each function ######################################################
 
@@ -31,4 +31,4 @@ sims_preprocessed <- preprocess_output(saveData,folder)
 power_table <- summary_stats(saveData,nFrom,nTo,nBy,folder)
 
 ## 5_plot_results -------------------------------------------------------------
-# plot_results(folder)
+plot_results(folder)
